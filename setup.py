d31e5f6630f9ed8a9eb9d660cecfc390eb909909
Mathias Leimgruber <m.leimgruber@4teamwork.ch>
2010-07-12 13:19:43 +0000

    fix path to version.txt

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = open('izug/blog/version.txt').read().strip()
+version = open('simplelayout/types/common/version.txt').read().strip()
 
 setup(name='simplelayout.types.common',
       version=version,
