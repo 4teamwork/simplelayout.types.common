0746e59ffa0742d901f46bc081df031ae466d1eb
Jonas Baumann <jone@jone.ch>
2012-10-26 16:05:45 +0200

    Rename readme to .rst.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = open('simplelayout/types/common/version.txt').read().strip()
 setup(name='simplelayout.types.common',
       version=version,
       description="simplelayout common types",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
